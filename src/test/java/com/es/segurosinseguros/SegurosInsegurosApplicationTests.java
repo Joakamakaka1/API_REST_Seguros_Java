20057716c9c03df61e1649db792f64246be8f1ba
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2024-11-18 23:36:34 +0100

    Test

--- src/test/java/com/es/segurosinseguros/SegurosInsegurosApplicationTests.java
+++ src/test/java/com/es/segurosinseguros/SegurosInsegurosApplicationTests.java
@@ -1,13 +1,9 @@
 package com.es.segurosinseguros;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SegurosInsegurosApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
